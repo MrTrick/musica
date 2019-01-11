104d75111f6b65ee0eec5adca35a5f4b754e6a59
MrTrick <github@mindbleach.com>
2019-01-11 17:57:42 +1100

    Updates

---
+++ precache-manifest.1975d8d58264c97d88eb1a3791b8fe34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc3681205d4d2abda71670fd544c10d1",
+    "url": "/musica/static/media/logo.fc368120.svg"
+  },
+  {
+    "revision": "a8e86bf58588d7b57105",
+    "url": "/musica/static/js/runtime~main.a8e86bf5.js"
+  },
+  {
+    "revision": "6d6c6a8e0e75dd270fbe",
+    "url": "/musica/static/js/main.6d6c6a8e.chunk.js"
+  },
+  {
+    "revision": "ed5a697648d3f78800e6",
+    "url": "/musica/static/js/1.ed5a6976.chunk.js"
+  },
+  {
+    "revision": "47c9bac6482296c78e17011e4eca1062",
+    "url": "/musica/index.html"
+  }
+];
