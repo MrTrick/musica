9495d1794c832208acfb74eed2022849615dcf4c
MrTrick <github@mindbleach.com>
2019-01-22 23:10:43 +1100

    Updated tests, coverage on InputSearch

--- client/src/components/InputSearch.test.js
+++ client/src/components/InputSearch.test.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import InputSearch from './InputSearch';
+import React from "react";
+import ReactDOM from "react-dom";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<InputSearch
-    value=''
-    handleChange={()=>{}}
-  />, div);
+import InputSearch from "./InputSearch";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<InputSearch value="" handleChange={() => {}} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-//TODO more tests
+it('Inserts "value" into the component, clear icon appears', () => {
+  const wrapper = mount(
+    <InputSearch value="" handleChange={() => {}} />
+  );
+  expect(wrapper).toMatchSnapshot();
+  expect(wrapper.find('input').prop('value')).toBe('');
+  expect(wrapper.find('ClearIcon')).toHaveLength(0);
+
+  wrapper.setProps({value: "Rap"});
+  expect(wrapper).toMatchSnapshot();
+  expect(wrapper.find('input').prop('value')).toBe('Rap');
+  expect(wrapper.find('ClearIcon')).toHaveLength(1);
+});
+
+it('Calls "handleChange" when the input content changes, or the "clear" button is clicked.', () => {
+  const onChangeMock = jest.fn();
+  const wrapper = mount(
+    <InputSearch value="oldvalue" handleChange={onChangeMock} />
+  );
+  const event = { target: { value: 'newvalue' } };
+  wrapper.find('input').simulate('change', event);
+  expect(onChangeMock).toBeCalledWith('newvalue');
+
+  const clear = wrapper.find('ClearIcon');
+  clear.simulate('click');
+  expect(onChangeMock).toBeCalledWith('');
+});
--- client/src/components/Logo.test.js
+++ client/src/components/Logo.test.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Logo from './Logo';
+import React from "react";
+import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
+import Logo from "./Logo";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<Logo />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-//TODO more tests
+it("looks like a logo should", () => {
+  const component = renderer.create(<Logo />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
